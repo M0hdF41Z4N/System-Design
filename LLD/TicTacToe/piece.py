201d359b23c86de4f75d3b1940733fad589d0f1f
Mohd Faizan <31098815+M0hdF41Z4N@users.noreply.github.com>
2025-05-20 16:58:38 +0530

    Update piece.py

--- LLD/TicTacToe/piece.py
+++ LLD/TicTacToe/piece.py
@@ -1,3 +1,18 @@
+from enum import Enum
+
+class PieceType(Enum):
+    X = 'X'
+    O = 'O'
+
 class Piece:
   def __init__(self,pieceType):
     self.pieceType =  pieceType
+
+
+class PieceO(Piece):
+  def __init__():
+    super.__init__(pieceType.O)
+
+class PieceX(Piece):
+  def __init__():
+    super.__init__(pieceType.X)

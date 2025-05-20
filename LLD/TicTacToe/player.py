0d2f513745815e91d7d6297d1a8374857c5841c3
Mohd Faizan <31098815+M0hdF41Z4N@users.noreply.github.com>
2025-05-20 16:26:11 +0530

    Update player.py

--- LLD/TicTacToe/player.py
+++ LLD/TicTacToe/player.py
@@ -1,1 +1,17 @@
+class Player:
+  def __init__(self,name,piece):
+    self.name = name
+    self.piece = piece
 
+
+  def getName():
+    return self.name
+
+  def getPlayingPiece():
+    return self.piece
+
+  def setPlayingPiece(piece):
+    self.piece = piece
+
+  def setName(name):
+    self.name = name
